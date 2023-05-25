879100f34401eedfbd08c48211afad9c59da5a46
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-05-25 15:49:35 +0100

    configure internationalizm and add translation files

---
+++ src/main/java/com/example/hireme/TestController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.hireme;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ *
+ * @author KAOUTAR
+ */
+@Controller
+@RequestMapping("/greeting")
+public class TestController {
+
+
+    @GetMapping
+    public String getSource(@RequestHeader("Accept-Language") String locale){
+            //return languagesConfig.messageSource().getMessage("greeting", null,new Locale(locale));
+            return "test";
+    }
+    
+}
