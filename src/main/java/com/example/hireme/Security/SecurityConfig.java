8d7d4454c578cae5a3cfbe302c1b2df9c6df9a16
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-05-27 19:28:59 +0100

    remove conflect files

--- src/main/java/com/example/hireme/Security/SecurityConfig.java
+++ src/main/java/com/example/hireme/Security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.hireme.Security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -13,29 +18,22 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        http.csrf().disable().authorizeRequests().antMatchers("/","/login","/css/**","/js/**").permitAll()
-//                .and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/",true)
-//                .passwordParameter("password")
-//                .usernameParameter("username")
-//                .and().rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)).key("somethingverysecured").rememberMeParameter("remember-me")
-//                .and ().logout().logoutUrl("/logout").clearAuthentication (true).invalidateHttpSession(true).deleteCookies ("JSESSIONID", "remember-me")
-//                .logoutSuccessUrl("/login");
-//        return http.build();
-
-        /*return http.httpBasic(httpBasic -> {} ).authorizeHttpRequests(authorizeHttpRequests ->
-                authorizeHttpRequests.requestMatchers("login","/login","/assets/**","/sass/**","/scss/**").permitAll()).formLogin(formLogin -> formLogin.loginPage("/login").permitAll()).build();
-   */
         return http.csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.loginPage("/login").permitAll().defaultSuccessUrl("/",true))
                 .authorizeHttpRequests (
                         authorizeConfig -> {
-                            authorizeConfig.requestMatchers(HttpMethod.POST,"/registration/**").permitAll();
-                            authorizeConfig.requestMatchers(HttpMethod.GET,"/registration/**").permitAll();
+                            authorizeConfig.requestMatchers("/registration/**").permitAll();
                             authorizeConfig.requestMatchers("/","/login","/registration/**","/assets/**","/sass/**","/scss/**").permitAll();
                         //authorizeConfig.anyRequest().authenticated();
-        }).build();
+        }).logout(logout -> logout.logoutUrl("/logout").clearAuthentication(true)
+                        .deleteCookies("JSESSIONID").logoutSuccessUrl("/")).build();
     }
+
+
+
+
 }
--- src/main/java/com/example/hireme/Service/UserService.java
+++ src/main/java/com/example/hireme/Service/UserService.java
@@ -2,11 +2,20 @@ package com.example.hireme.Service;
 
 import com.example.hireme.Repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserService {
+public class UserService  implements UserDetailsService{
     private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findStudentByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User with email "+email+" not found!"));
+
+    }
 
 }
