79878ae61eafe28e3512977964cf758238ac50d3
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-06-06 18:49:52 +0100

    display login/signup buttons just if user not authantified yet + apply translation on home page + customize view

--- src/main/java/com/example/hireme/Controller/HomeController.java
+++ src/main/java/com/example/hireme/Controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.example.hireme.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String getHomePage(){
+    public String getHomePage(Authentication authentication, Model model){
+        model.addAttribute("user",authentication);
         return "home";
     }
 
