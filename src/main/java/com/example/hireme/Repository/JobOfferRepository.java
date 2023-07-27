6468fcd1b02b0e59b980368eff320343b14cabed
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-07-27 14:55:32 +0100

    commit

--- src/main/java/com/example/hireme/Repository/JobOfferRepository.java
+++ src/main/java/com/example/hireme/Repository/JobOfferRepository.java
@@ -17,16 +17,16 @@ public interface JobOfferRepository extends JpaRepository<JobOffer,Long> {
     @Query(value = "select count(jobs.id),companies.name from jobs_offers jobs,companies where jobs.company_id=companies.id group by companies.name", nativeQuery = true)
     List<?> jobsInsights();
 
-    @Query(value = "select * from jobs_offers jobs where jobs.company_id=?1 limit ?2,?3", nativeQuery = true)
+    @Query(value = "select * from jobs_offers jobs where jobs.company_id=?1  limit ?2,?3", nativeQuery = true)
     List<JobOffer> findByCompanyIdWithPagination(Long company_id,long start,long end);
 
-    @Query(value = "select * from jobs_offers jobs where active=b'1' order by published_at desc limit 4", nativeQuery = true)
+    @Query(value = "select jobs.* from jobs_offers jobs,companies c where jobs.company_id=c.id and jobs.active=b'1' and c.active=b'1' order by published_at desc limit 4", nativeQuery = true)
     List<JobOffer> findRecentJobs();
 
-    @Query(value = "select * from jobs_offers jobs where active=b'1' and jobs.title like %?1% and category_id like ?3 and city_id in(select id from cities where country_id like ?2)", nativeQuery = true)
+    @Query(value = "select jobs.* from jobs_offers jobs,companies c where jobs.company_id=c.id and jobs.active=b'1' and c.active=b'1' and jobs.title like %?1% and jobs.category_id like ?3 and jobs.city_id in(select id from cities where country_id like ?2)", nativeQuery = true)
     List<JobOffer> findByTitleAndCategoryAndLocation(String title,String location,String category);
 
-    @Query(value = "select * from jobs_offers jobs where active=b'1' and jobs.title like %?1% and category_id like ?3 and city_id in(select id from cities where country_id like ?2) limit ?4,?5", nativeQuery = true)
+    @Query(value = "select jobs.* from jobs_offers jobs,companies c where jobs.company_id=c.id and jobs.active=b'1' and c.active=b'1' and jobs.title like %?1% and jobs.category_id like ?3 and jobs.city_id in(select id from cities where country_id like ?2) limit ?4,?5", nativeQuery = true)
     List<JobOffer> findByTitleAndCategoryAndLocationWithPagination(String title,String location,String category,long start,long end);
 
     @Query(value = "select * from jobs_offers jobs where jobs.id in(select offer_id from candidatures where candidate_profile_id=?1)", nativeQuery = true)
